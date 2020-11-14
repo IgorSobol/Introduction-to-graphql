e85d59a3131512b618960347ecc737aef3491da2
Igor Sobol <sobol.dominus@gmail.com>
2020-11-14 19:31:22 +0200

    Added second theme

--- schema/schems.js
+++ schema/schems.js
@@ -1,11 +1,24 @@
-const {GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLID} = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt
+} = require('graphql');
 
 const movies = [
-  { id: '1', name: 'Pulp Fiction', genre: 'Crime' },
-  { id: '2', name: '1984', genre: 'Sci-Fi' },
-  { id: 3, name: 'V for vendetta', genre: 'Sci-Fi-Triller' },
-  { id: 4, name: 'Snatch', genre: 'Crime-Comedy' },
-]
+  { id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1', },
+  { id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2', },
+  { id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3', },
+  { id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4', },
+];
+
+const directors = [
+  { id: '1', name: 'Quentin Tarantino', age: 55 },
+  { id: '2', name: 'Michael Radford', age: 72 },
+  { id: '3', name: 'James McTeigue', age: 51 },
+  { id: '4', name: 'Guy Ritchie', age: 50 },
+];
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
@@ -13,24 +26,44 @@ const MovieType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    director: {
+      type: DirectorType,
+      resolve(parent, args) {
+        return directors.find(director => director.id === parent.id);
+      }
+    }
   }),
-})
+});
+
+const DirectorType = new GraphQLObjectType({
+  name: 'Director',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
+});
 
 const Query = new GraphQLObjectType({
   name: 'Query',
   fields: {
     movie: {
       type: MovieType,
-      args: {
-        id: { type: GraphQLID },
-      },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return movies.find( movie => movie.id = args.id )
-      }
-    }
+        return movies.find(movie => movie.id === args.id);
+      },
+    },
+    director: {
+      type: DirectorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return directors.find(director => director.id === args.id);
+      },
+    },
   }
-})
+});
 
 module.exports = new GraphQLSchema({
   query: Query,
-})
+});
