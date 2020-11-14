25b5e5f3279f95792975db82bf3d975536efad0d
Igor Sobol <sobol.dominus@gmail.com>
2020-11-14 10:43:38 +0200

    Added first schema graphql and first query

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('../schema/schems')
 
 const app = express();
 const PORT = 3005;
 
-app.use('/graphql', graphqlHTTP({ graphiql: true}));
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
 
 app.listen(PORT, err => {
   err ? console.log(err) : console.log('Server started!');
